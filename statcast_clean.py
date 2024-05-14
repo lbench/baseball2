c172a0e9539876089f51d3343c533e595762609f
lbench <liammbench@gmail.com>
2024-05-13 22:10:13 -0400

    First commit

--- statcast_clean.py
+++ statcast_clean.py
@@ -1,59 +1,65 @@
 import os
 import pandas as pd
 
-read_directory = 'raw_csvs'
-read_file = 'statcast_raw.csv'
-write_directory = 'cleaned_csvs'
-write_file = 'statcast_cleaned.csv'
 
-path = os.path.join(read_directory, read_file)
+def statcast_clean():
+    read_directory = 'raw_csvs'
+    read_file = 'statcast_raw.csv'
+    write_directory = 'cleaned_csvs'
+    write_file = 'statcast_cleaned.csv'
 
-relevant_attributes = ['player_name',
-                       'pitcher',
-                       'game_date',
-                       'game_pk',
-                       'at_bat_number',
-                       'pitch_number',
-                       'pitch_type',
-                       'pitch_name',
-                       'release_speed',
-                       'release_pos_x',
-                       'release_pos_z',
-                       'zone',
-                       'p_throws',
-                       'type',
-                       'pfx_x',
-                       'pfx_z',
-                       'plate_x',
-                       'plate_z',
-                       'vx0',
-                       'vy0',
-                       'vz0',
-                       'ax',
-                       'ay',
-                       'az',
-                       'sz_top',
-                       'sz_bot',
-                       'effective_speed',
-                       'release_spin_rate',
-                       'release_extension',
-                       'release_pos_y',
-                       'spin_axis',
-                       ]
+    path = os.path.join(read_directory, read_file)
 
-df = pd.read_csv(path)
+    relevant_attributes = ['player_name',
+                           'pitcher',
+                           'game_date',
+                           'game_pk',
+                           'at_bat_number',
+                           'pitch_number',
+                           'pitch_type',
+                           'pitch_name',
+                           'release_speed',
+                           'release_pos_x',
+                           'release_pos_z',
+                           'zone',
+                           'p_throws',
+                           'type',
+                           'pfx_x',
+                           'pfx_z',
+                           'plate_x',
+                           'plate_z',
+                           'vx0',
+                           'vy0',
+                           'vz0',
+                           'ax',
+                           'ay',
+                           'az',
+                           'sz_top',
+                           'sz_bot',
+                           'effective_speed',
+                           'release_spin_rate',
+                           'release_extension',
+                           'release_pos_y',
+                           'spin_axis',
+                           ]
 
-df = df[relevant_attributes]
-df = df.reindex(index=df.index[::-1])
+    df = pd.read_csv(path)
 
-df['pitch_of_game'] = df.groupby('game_pk').cumcount() + 1
+    df = df[relevant_attributes]
+    df = df.reindex(index=df.index[::-1])
 
-col = list(df.columns)
-col.insert(4, col.pop())
-df = df[col]
+    df['pitch_of_game'] = df.groupby('game_pk').cumcount() + 1
 
-df = df[df['release_speed'].notna()]
+    col = list(df.columns)
+    col.insert(4, col.pop())
+    df = df[col]
 
-to_write = os.path.join(write_directory, write_file)
+    df = df[df['release_speed'].notna()]
 
-df.to_csv(to_write, index=False)
+    to_write = os.path.join(write_directory, write_file)
+
+    df.to_csv(to_write, index=False)
+
+
+if __name__ == '__main__':
+    statcast_clean()
